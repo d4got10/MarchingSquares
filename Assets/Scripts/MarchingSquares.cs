73ce2c349d67d6841f1b5ac8d3bc15013f0bc41d
d4got10 <izotov.da@students.dvfu.ru>
2020-07-31 16:23:08 +1000

    switched to 2019.3.2f1 version of Unity

---
+++ Assets/Scripts/MarchingSquares.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+public class MarchingSquares : MonoBehaviour 
+{
+    private void Awake()
+    {
+
+    }
+}
